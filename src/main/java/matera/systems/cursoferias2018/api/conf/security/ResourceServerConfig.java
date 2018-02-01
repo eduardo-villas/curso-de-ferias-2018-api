832d4b61fba9c2d388c09fda1b4d02600de6d52f
Junior Miqueletti <junior.miqueletti@matera.com>
2018-02-01 21:32:09 -0200

    added swagger

--- src/main/java/matera/systems/cursoferias2018/api/conf/security/ResourceServerConfig.java
+++ src/main/java/matera/systems/cursoferias2018/api/conf/security/ResourceServerConfig.java
@@ -29,6 +29,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/health").permitAll()
+                .antMatchers("/swagger-ui").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().csrf().disable();
