b8b5a56c417c8e2a1ee724efad87904908a263e9
Junior Miqueletti <junior.miqueletti@matera.com>
2018-02-01 21:54:25 -0200

    fix

--- src/main/java/matera/systems/cursoferias2018/api/filter/CorsFilter.java
+++ src/main/java/matera/systems/cursoferias2018/api/filter/CorsFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
@@ -41,11 +42,20 @@ public class CorsFilter implements Filter {
 			throws IOException, ServletException {
 		
 		HttpServletResponse response = (HttpServletResponse) res;
+		HttpServletRequest request = (HttpServletRequest) req;
 		
-		response.setHeader("Access-Control-Allow-Methods", "GET, DELETE, POST, PUT, OPTIONS");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Headers",
+				"X-Requested-With, Authorization, Accept-Language, Content-Type");
+		response.setHeader("Access-Control-Allow-Methods", "GET, DELETE, OPTIONS, POST, PUT");
 		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Max-Age", "3600");
 
-		chain.doFilter(req, res);
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(req, res);
+		}
 	}
 
 	@Override
